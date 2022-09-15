a5a4112fe241dcd38b5a83fd07e69a8e877ae9d7
lanzarot <luis-389@hotmail.com>
2022-09-15 17:43:24 -0500

    finish  CRUD

--- src/main/java/com/lanzarotdev/app/rest/Controller/TodoController.java
+++ src/main/java/com/lanzarotdev/app/rest/Controller/TodoController.java
@@ -3,10 +3,7 @@ package com.lanzarotdev.app.rest.Controller;
 import com.lanzarotdev.app.rest.Model.Task;
 import com.lanzarotdev.app.rest.Repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,14 +17,34 @@ public class TodoController {
         return "Hola Mundo!!!";
     }
 
+    // obtener tareas
     @GetMapping(value= "/tasks")
     public List<Task> getTasks(){
         return todoRepository.findAll();
     }
+
+    // agregar y colocar una nueva task
     @PostMapping(value="/savetask")
     public String saveTask(@RequestBody Task task){
         todoRepository.save(task);
         return "saved task";
+    }
+
+    // para editar y actualizar dependiendo del id que envie
+    @PutMapping(value="/update/{id}")
+    public String updateTask(@PathVariable long id, @RequestBody Task task) {
+        Task updatedTask = todoRepository.findById(id).get(); // la variable updatedTask de tipo Task va a buscar en el almacenamiento y obtener el cuerpo por el id enviado
+        updatedTask.setTitle(task.getTitle()); // seteo nuevo titulo
+        updatedTask.setDescription(task.getDescription()); // seteo nueva descripción obteniendo primero la descripción que quiero editar
+        todoRepository.save(updatedTask);
+        return "Updated Task";
+    }
+
+    @DeleteMapping(value="delete/{id}")
+    public String deleteTask(@PathVariable long id) {
+        Task deletedTask = todoRepository.findById(id).get();
+        todoRepository.delete(deletedTask);
+        return "Deleted Task";
     }
 
 }
