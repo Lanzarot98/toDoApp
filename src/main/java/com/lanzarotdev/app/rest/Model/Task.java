24c746a09cf77e188f35cf5839bc51d56b954eef
lanzarot <luis-389@hotmail.com>
2022-09-15 17:56:49 -0500

    fix main

--- src/main/java/com/lanzarotdev/app/rest/Model/Task.java
+++ src/main/java/com/lanzarotdev/app/rest/Model/Task.java
@@ -1,16 +1,24 @@
 package com.lanzarotdev.app.rest.Model;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.*;
 
-@EntityScan
+@Entity
+@Table(schema = "TasksCrud")
 public class Task {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column
     private String title;
+    @Column
     private String description;
 
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
@@ -18,13 +26,15 @@ public class Task {
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getDescription(){
+    public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
