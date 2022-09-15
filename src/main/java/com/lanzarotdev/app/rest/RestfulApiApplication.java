99c05252e99016f557afda27d83119bb79211f4d
lanzarot <luis-389@hotmail.com>
2022-09-15 15:51:47 -0500

    fix the project

---
+++ src/main/java/com/lanzarotdev/app/rest/RestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.lanzarotdev.app.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+
+}
